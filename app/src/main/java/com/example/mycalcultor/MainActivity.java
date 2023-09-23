cf8cd531a7d4c4a9c9defc773ffb734921433f89
NetflixClone <hosiduc2002.chem@gmail.com>
2023-09-23 18:30:42 +0700

    change UI

--- app/src/main/java/com/example/mycalcultor/MainActivity.java
+++ app/src/main/java/com/example/mycalcultor/MainActivity.java
@@ -3,15 +3,12 @@ package com.example.mycalcultor;
 
 import android.app.Activity;
 import android.content.Context;
-import android.media.audiofx.Equalizer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
